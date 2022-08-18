b5d9482d0a3413e1382161e44c23d36cef0c34d9
seongwoon jeong <ggogumalove@gmail.com>
2022-08-19 08:47:41 +0900

    week4

---
+++ week4/factory.py
@@ -0,0 +1,15 @@
+
+# Factory, SingleTon
+
+# 팩토리 패턴
+
+
+1. problem based learn - aaron
+2. order - item 구조 - ben
+3. DB Conenction을 관리하는 객체를 Factory로 생성하기 - Lumi
+
+# Singleton pattern
+1. Singleton DB Model - Lumi
+2. Redis connection with singleton - Aaron
+3. Python에서는 모듈이 Singleton으로 관리되기 때문에 새로운 파일로 정의해서 사용하자. - Ben
+    - Enum으로도 사용 가능
